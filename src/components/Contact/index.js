e080f36e9f67fddff8385d90531964c0102fe41a
Tallia-Anne <erinawambiekele@gmail.com>
2022-09-30 18:03:13 +0200

    testcontact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,32 +1,32 @@
 import './index.css'
 import contactimage from '../../assets/online-web-design.jpg';
 
-// import React, { useState } from 'react';
+ import React, { useState } from 'react';
 
 const Contact = () => {
-    // const [name, setName] = useState('');
-    // const [email, setEmail] = useState('');
-    // const [message, setMessage] = useState('');
+     const [sujet, setName] = useState('');
+     const [email, setEmail] = useState('');
+     const [message, setMessage] = useState('');
     
 
-    // const submitRequest = async (e) => {
-    //     e.preventDefault();
-    //     console.log({ name, email, message });
-    //     const response = await fetch("http://localhost:3000/contact/sendcontact", {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify({  name, email, message })
-    //     });
-    //     const resData = await response.json();
-    //     if (resData.status === 'success') {
-    //         alert("Message Sent.");
-    //         this.resetForm()
-    //     } else if (resData.status === 'fail') {
-    //         alert("Message failed to send.")
-    //     }
-    // };
+     const submitRequest = async (e) => {
+         e.preventDefault();
+        console.log({ sujet, email, message });
+         const response = await fetch("http://localhost:3000/contact/sendcontact", {
+             method: 'POST',
+             headers: {
+                 'Content-type': 'application/json'
+             },
+             body: JSON.stringify({  sujet, email, message })
+         });
+         const resData = await response.json();
+         if (resData.status === 'success') {
+             console.log("Message Sent.");
+             this.resetForm()
+         } else if (resData.status === 'fail') {
+             alert("Message failed to send.")
+         }
+     };
     
     
     return (
@@ -35,27 +35,27 @@ const Contact = () => {
             
             <div className="form-contact">
                 
-                <form  >
+                <form onSubmit={submitRequest}  >
                     
                     <h1>Contacter-moi</h1>
                     
                     <div className="input-form">
-                        <input type="name" id="name" name="name" placeholder="ton name" className='inform'
-                            // onChange={e => setName(e.target.value)}
-                            // value={name}
+                        <input type="sujet" id="sujet" name="sujet" placeholder="ton sujet" className='inform'
+                             onChange={e => setName(e.target.value)}
+                             value={sujet}
                         />
                     </div>
                     <div className="input-form">
                         <input type="email" id="email" name="email" placeholder="ton email" className='inform'
-                            // onChange={e => setEmail(e.target.value)}
-                            // value={email}
+                             onChange={e => setEmail(e.target.value)}
+                             value={email}
                         />
                     </div>
                     
                     <div className="input-form">
                         <textarea name="comment" id="message" cols="30" rows="10" required placeholder='Entrer votre message'
-                            // onChange={e => setMessage(e.target.value)}
-                            // value={message}
+                             onChange={e => setMessage(e.target.value)}
+                            value={message}
                         ></textarea>
                     </div>
                     
