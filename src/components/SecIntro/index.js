6ce138abb63f0514ea928f8a244cf4f34a9cd5ad
Tallia-Anne <erinawambiekele@gmail.com>
2022-10-17 23:55:30 +0200

    migration

--- src/components/SecIntro/index.js
+++ src/components/SecIntro/index.js
@@ -10,7 +10,7 @@ const Sectintro = () => {
                 <p> Je suis une développeuse web junior depuis 2 ans.Diplômé du titre professionnel de Développeur web et web mobile du bac +2. Je vous souhaite une bienvenue sur mon portfolio ! Pour vous raconter mon histoire et mon parcours.
                     Cela va vous montrer la passion dans le monde du développement web.
                     Avec une citation : "La créativité est une fleur si délicate que,  bien que les compliments la fassent s'épanouir, le découragement peut l'empêcher
-                    de fleurir." Qui vient Alex Osborn. Grâce au développement, web m'a permet de laisser entrer inventivité. Vous allez explorer dans mon monde  rempli de créativité et d'imagination.
+                    de fleurir." Qui vient Alex Osborn. Grâce au développement, web m'a permet de laisser entrer inventivité, ma sensiblité et aux niveau de couleur. Vous allez explorer dans mon monde rempli de créativité et d'imagination.
                     Ce projet me tient à d'illustrer mes idées et à créer.
                     Je vous invite à y découvrir les projets, dont les sites web, les projets design. Aussi me découvrir la programmeuse web que je suis. Vous pouvez me faire confiance.
                     
