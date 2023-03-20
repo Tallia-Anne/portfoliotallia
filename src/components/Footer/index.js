a14e3c46c22c14e971f0ee9c1a55fff091405a7a
Tallia-Anne <erinawambiekele@gmail.com>
2023-03-20 15:20:25 +0100

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,29 +1,29 @@
 import './index.css'
 const Footer = () => {
-    
+
     return (
-        
+
         <footer>
-            
+
             <div className="social">
-                
+
                 <a href="google.com"><i class="fab fa-instagram"></i></a>
                 <a href="https://www.behance.net/erinawambiekele"><i class="fa-brands fa-behance"></i></a>
                 <a href="https://github.com/Tallia-Anne?tab=repositories"><i class="fa fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/tallia-anne-wambiekele-kiyeko-36a1451ba/"><i class="fa-brands fa-linkedin"></i></a>
-                
-                
+
+
             </div>
-            
+
             <p className="copyright">Wambiekele Kiyeko Tallia-Anne @2022</p>
-            
-            
+
+
         </footer>
-        
-)
-    
-    
-    
+
+    )
+
+
+
 }
 
 export default Footer
