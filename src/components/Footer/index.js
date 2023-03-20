5662e9b73e6b290030194b471353f9f6b5fd6047
Tallia-Anne <erinawambiekele@gmail.com>
2023-03-20 15:16:45 +0100

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 
                 <a href="google.com"><i class="fab fa-instagram"></i></a>
                 <a href="https://www.behance.net/erinawambiekele"><i class="fa-brands fa-behance"></i></a>
-                <a href="https://github.com/Tallia-Anne?tab=repositories"><i class="fa-brands fa-github"></i></a>
+                <a href="https://github.com/Tallia-Anne?tab=repositories"><i class="fa fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/tallia-anne-wambiekele-kiyeko-36a1451ba/"><i class="fa-brands fa-linkedin"></i></a>
                 
                 
